5bef9d9b8531783029e56420daa8e1cd09e5527e
jconcha56429 <78937358+jconcha56429@users.noreply.github.com>
2021-06-17 16:46:04 -0700

    fix bug where scripters were out of order

--- UFO-level-1/js/app.js
+++ UFO-level-1/js/app.js
@@ -1,5 +1,20 @@
 // from data.js
 var tableData = data
+var rows = tableData.map(sighting => sighting )
+var row1 = rows[0]
+indexes = Object.keys(row1);
+html_table = d3.select("#main_table")   
+var table_head = html_table.append("th").text();
+rows.forEach(function(x) {
+    //var table_head = html_table.append("th").text('caca');
 
-
-console.log(tableData)
+    var row = html_table.append("tr");
+    var date = row.append("td").text(Object.values(x)[0]);
+    var place = row.append("td").text(Object.values(x)[1]);
+    var state = row.append("td").text(Object.values(x)[2]);
+    var fourth = row.append("td").text(Object.values(x)[3]);
+    var fifth = row.append("td").text(Object.values(x)[4]);
+    var sixth = row.append("td").text(Object.values(x)[5]);
+    var seveneth = row.append("td").text(Object.values(x)[6]);
+    var eight = row.append("td").text(Object.values(x)[7]);
+});
