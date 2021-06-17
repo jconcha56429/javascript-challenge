ad8025a081f6bcd451090160ecc1e4a337239488
jconcha56429 <78937358+jconcha56429@users.noreply.github.com>
2021-06-17 16:54:28 -0700

    added header tag to th in table

--- UFO-level-1/js/app.js
+++ UFO-level-1/js/app.js
@@ -4,10 +4,13 @@ var rows = tableData.map(sighting => sighting )
 var row1 = rows[0]
 indexes = Object.keys(row1);
 html_table = d3.select("#main_table")   
-var table_head = html_table.append("th").text();
+
+//var table_head = html_table.append("th").text('');
+indexes.forEach(function(i) {
+    var header = html_table.append("th").text(i)
+})
 rows.forEach(function(x) {
     //var table_head = html_table.append("th").text('caca');
-
     var row = html_table.append("tr");
     var date = row.append("td").text(Object.values(x)[0]);
     var place = row.append("td").text(Object.values(x)[1]);
