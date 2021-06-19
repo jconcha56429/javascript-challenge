fcc8b5b50a4cf9f358bffde374e645e4f9780468
jconcha56429 <78937358+jconcha56429@users.noreply.github.com>
2021-06-18 20:18:29 -0700

    data added and begin html page

--- UFO-level-1/js/app.js
+++ UFO-level-1/js/app.js
@@ -5,12 +5,10 @@ var row1 = rows[0]
 indexes = Object.keys(row1);
 html_table = d3.select("#main_table")   
 
-//var table_head = html_table.append("th").text('');
 indexes.forEach(function(i) {
     var header = html_table.append("th").text(i)
 })
 rows.forEach(function(x) {
-    //var table_head = html_table.append("th").text('caca');
     var row = html_table.append("tr");
     var date = row.append("td").text(Object.values(x)[0]);
     var place = row.append("td").text(Object.values(x)[1]);
